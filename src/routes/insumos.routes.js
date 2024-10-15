86f7368190e9c37abb716c3b9759593f0dd24590
AnaisLaguna <annslaguna@gmail.com>
2024-10-14 21:45:27 -0600

    insumos incompletos

--- src/routes/insumos.routes.js
+++ src/routes/insumos.routes.js
@@ -1,10 +1,18 @@
 import express from "express";
-
-import {getInsumos, postInsumos} from "../controllers/insumos.controller.js";
+import { getInsumos, postInsumos, patchInsumo, deleteInsumo } from "../controllers/insumos.controller.js";
 
 const router = express.Router();
 
-router.get('/insumos', getInsumos)
-router.post('/insumos', postInsumos)
+// Ruta para obtener todos los insumos
+router.get('/insumos', getInsumos);
 
-export default router;  //exportamos el router para poder usarlo en otros archivos.  //
+// Ruta para crear un nuevo insumo
+router.post('/insumos', postInsumos);
+
+// Ruta para actualizar un insumo por ID
+router.patch('/insumos/:id', patchInsumo);
+
+// Ruta para eliminar un insumo por ID
+router.delete('/insumos/:id', deleteInsumo);
+
+export default router;  // exportamos el router para poder usarlo en otros archivos.
